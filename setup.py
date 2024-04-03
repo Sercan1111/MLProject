5be68c856caedc902230565de841cf6e58f63829
Sercan ARABACI <sercanarabaci244@gmail.com>
2024-04-03 20:23:12 +0200

    project structure

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import find_packages,setup
-
+from typing import List
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+    
+    ''''''' this function will return the list of get_requirements'''''''
+    
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ")for req in requirements.txt]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+        
 
 setup(
 name = 'MLProject',
@@ -7,6 +20,6 @@ version = '0.0.1',
 author = 'Sercan',
 author_email= 'sercanarabaci1@outlook.com',
 packages=find_packages(),
-install_requires = ['pandas','numpy','seaborn']
+install_requires = get_requirements('requirements.txt')
 
 )
